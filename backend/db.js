fb75f3d15b9052809a776eef662c922d3a05ec0b
secondglitter <Joshgonzbv@gmail.com>
2025-07-02 17:48:40 -0500

    cambios en db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
-require('dotenv').config(); // 👈 Asegúrate de cargar variables de entorno
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || '10'),
-  queueLimit: 0
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
 });
 
 module.exports = pool;
